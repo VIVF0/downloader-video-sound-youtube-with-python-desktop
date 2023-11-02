fddcd0f38014a778aab4f67debd07ebd9ed54932
Vitor Vieira Freire <vitorvfreire@outlook.com>
2023-11-02 17:09:05 -0300

    .

---
+++ Client/download_audio.py
@@ -0,0 +1,4 @@
+from src import Audio, Video
+
+audio = Audio(url='https://www.youtube.com/watch?v=yWjCs-wD7Eo', path="D:\VITOR")
+audio.download_audio()
--- Client/src/audio.py
+++ Client/src/audio.py
@@ -1,6 +1,5 @@
 from pytube import YouTube
 from pytube import Playlist
-import moviepy.editor as mp
 import re
 import os
 
@@ -20,11 +19,11 @@ class Audio:
     def download_audio(self):
         yt = YouTube(self.url)
         ys = yt.streams.filter(only_audio=True).first().download(self.path)
-        for file in os.listdir(self.path):
+        """for file in os.listdir(self.path):
             if re.search('mp4', file):                                 
                 mp4_path = os.path.join(self.path , file)   
                 mp3_path = os.path.join(self.path, os.path.splitext(file)[0]+'.mp3') 
                 new_file = mp.AudioFileClip(mp4_path)  
                 new_file.write_audiofile(mp3_path)    
-                os.remove(mp4_path)
+                os.remove(mp4_path)"""
         print('Download Completo do Audio')
